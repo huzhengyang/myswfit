fd1a7e03fe8edf1a0442b7d82f47c30ca4cabbba
kadis <huzhengyangjob@qq.com>
2021-09-17 09:36:43 +0800

    new

---
+++ MySwfit/myswift/myTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  myTableViewController.swift
+//  myswift
+//
+//  Created by kadis on 2021/9/14.
+//
+
+import UIKit
+
+class myTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let btn = UIButton.init(type: .custom)
+        btn .setTitle("kadis", for: .normal)
+        self.view .addSubview(btn);
+        btn .addTarget(self, action: #selector(tapdd), for: .touchUpInside)
+        
+        
+        var boxx = UIView()  //kadis
+        
+        boxx.backgroundColor = .gray
+        
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    @objc func tapdd() {
+        print("")
+    }
+
+
+
+}
---
+++ MySwfit/myswift/ocViewController.m
@@ -0,0 +1,35 @@
+//
+//  ocViewController.m
+//  myswift
+//
+//  Created by kadis on 2021/9/14.
+//
+
+#import "ocViewController.h"
+
+@interface ocViewController ()
+
+@end
+
+@implementation ocViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+-(void)seyhello:(NSString *)name
+{
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
